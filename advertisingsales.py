62d0b058abbd9dfff0c52030c32b58263d8b2608
Hadiaminn <161563307+Hadiaminn@users.noreply.github.com>
2024-02-28 15:07:02 +0800

    Update advertisingsales.py

--- advertisingsales.py
+++ advertisingsales.py
@@ -22,9 +22,9 @@ This app predicts the **Sales** for type of advertising stratergy!
 st.sidebar.header('User Input Parameters')
 
 def user_input_features():
-    tv = st.sidebar.slider('TV', 4.3, 7.9, 5.4)
-    radio = st.sidebar.slider('Radio', 2.0, 4.4, 3.4)
-    newspaper = st.sidebar.slider('Newspaper', 1.0, 6.9, 1.3)
+    tv = st.sidebar.slider('TV', 0.7, 297, 100)
+    radio = st.sidebar.slider('Radio', 0, 50, 15)
+    newspaper = st.sidebar.slider('Newspaper', 0.3, 114, 20)
     data = {'TV': tv,
             'Radio': radio,
             'Newspaper': newspaper}
