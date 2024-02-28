fe03229b04b3fefa166cea55faecaa02a572ada2
Hadiaminn <161563307+Hadiaminn@users.noreply.github.com>
2024-02-28 15:07:48 +0800

    Update advertisingsales.py

--- advertisingsales.py
+++ advertisingsales.py
@@ -7,8 +7,6 @@ Original file is located at
     https://colab.research.google.com/drive/1DZdDDWD_HrFwl9XwvGodCGGzhZBRaULt
 """
 
-!pip install streamlit
-
 import streamlit as st
 import pandas as pd
 import pickle
